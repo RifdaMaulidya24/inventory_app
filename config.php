d486e7e36f69068bd151af06112f441f14d04cdf
Rifda Maulidya <rifdamaulidya24@gmail.com>
2025-11-23 19:04:02 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = getenv("DB_HOST"); 
+$user = getenv("DB_USER");    
+$pass = getenv("DB_PASS");      
+$db   = getenv("DB_NAME");       
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
